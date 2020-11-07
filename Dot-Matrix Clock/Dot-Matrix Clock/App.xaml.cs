018438ab0c914db13bc6072f6a807e9cc7403e17
Mikki <Mikki.solid@yandex.ru>
2020-11-07 23:22:06 +0500

    add project

---
+++ Dot-Matrix Clock/Dot-Matrix Clock/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dot_Matrix_Clock
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
